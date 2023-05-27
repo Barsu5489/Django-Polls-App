90f8a000eb9fb6409fc8821d771cab1aadf9ebfe
barsu5489 <ebarsulai@gmail.com>
2023-05-27 17:06:53 +0300

    routes and a migration

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("", view.index, name="index"),
+    path("", views.index, name="index"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,5 @@
-# from django.shortcuts import render
-from django.http import httpResponse
+from django.http import HttpResponse
 
-def index(request)
-return httpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
-# http://127.0.0.1:8000
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
