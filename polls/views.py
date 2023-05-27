9abc2a5e24bb09ae4974059d350d78d51fea5028
barsu5489 <ebarsulai@gmail.com>
2023-05-27 15:27:48 +0300

    views and how to call views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import httpResponse
 
+def index(request)
+return httpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+# http://127.0.0.1:8000
