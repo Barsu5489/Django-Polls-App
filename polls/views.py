efae5fb9f7ffd9b7ed96d1d3d021a6a4f50b060f
barsu5489 <ebarsulai@gmail.com>
2023-05-30 03:39:43 +0300

    A shortcut()

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,22 @@
-from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    ouput = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(ouput)
-
+    context = {
+       "latest_question_list": latest_question_list,
+   }
+    return render (request, "polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse("You're looking st question %s." % question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 def results(request, question_id):
     response = "You are looking at results of question %s."
-    return HttpResponse(response % question_id)
+    return render(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render("You're voting on question %s." % question_id)
